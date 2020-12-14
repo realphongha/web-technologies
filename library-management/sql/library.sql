0353d1a072cb9cb8959bb4b06105f257c577a705
PhongHH <realphongha@gmail.com>
2020-12-14 15:01:44 +0700

    update new sql statements

--- library-management/sql/library.sql
+++ library-management/sql/library.sql
@@ -27,29 +27,31 @@ CREATE TABLE IF NOT EXISTS `book` (
   `price` int(11) NOT NULL COMMENT 'Giá sách',
   `fee` int(11) NOT NULL COMMENT 'Giá mượn',
   `amount` int(11) NOT NULL COMMENT 'Số lượng còn lại',
+  `image` varchar(150) NOT NULL DEFAULT '/book/no-image.png',
   `status` tinyint(4) NOT NULL COMMENT 'Trạng thái sách. 0 - đã xóa, 1 - đang hoạt động.',
   `insert_date` datetime NOT NULL,
   `insert_by` int(11) NOT NULL,
   `update_date` datetime NOT NULL,
   `update_by` int(11) NOT NULL,
   PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COMMENT='Lưu thông tin sách';
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='Lưu thông tin sách';
 
--- Dumping data for table library.book: ~12 rows (approximately)
+-- Dumping data for table library.book: ~13 rows (approximately)
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` (`book_id`, `title`, `category`, `author`, `language`, `publisher`, `price`, `fee`, `amount`, `status`, `insert_date`, `insert_by`, `update_date`, `update_by`) VALUES
-	(1, 'book1', 'cut1', 'au1', 'lan2', 'pub1', 100000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-12-06 21:03:13', 1),
-	(2, 'book2', 'cat1', 'au2', 'lan1', 'pub1', 200000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-12-03 16:35:14', 1),
-	(3, 'book3', 'cat2', 'au3', 'lan1', 'pub3', 100000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-12-03 16:35:16', 1),
-	(4, 'book4', 'cat3', 'au1', 'lan2', 'pub3', 300000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-11-29 10:53:55', 1),
-	(5, 'book5', 'cat3', 'au4', 'lan1', 'pub2', 400000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-11-29 10:53:55', 1),
-	(6, 'book6', 'cat4', 'au1', 'lan3', 'pub4', 500000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-11-29 10:53:55', 1),
-	(7, 'book7', 'cat4', 'au1', 'lan1', 'pub1', 200000, 10000, 1000, 1, '2020-11-29 10:53:52', 1, '2020-12-02 11:14:13', 1),
-	(8, 'blah2', 'blah', 'blah', 'en', 'nxb1', 69000, 10000, 20000, 1, '2020-12-02 12:21:04', 1, '2020-12-03 16:35:12', 1),
-	(9, 'test', 'blah', 'blah', 'en', 'nxb1', 50000, 10000, 500, 1, '2020-12-02 12:26:52', 1, '2020-12-03 16:08:46', 1),
-	(10, 'blah23', 'blah', 'blah', 'en', 'nxb1', 69000, 10000, 10, 0, '2020-12-06 20:43:38', 1, '2020-12-06 21:26:16', 1),
-	(17, 'blah235', 'blah', 'blah', 'en', 'nxb1', 54000, 10000, 111, 1, '2020-12-07 11:22:17', 2, '2020-12-07 11:22:17', 2),
-	(18, 'test3434', 'cut1', 'blah', 'vn', 'vn', 50000, 5000, 900, 0, '2020-12-07 11:38:37', 2, '2020-12-07 13:16:36', 1);
+INSERT INTO `book` (`book_id`, `title`, `category`, `author`, `language`, `publisher`, `price`, `fee`, `amount`, `image`, `status`, `insert_date`, `insert_by`, `update_date`, `update_by`) VALUES
+	(1, 'book1', 'cut1', 'au1', 'lan2', 'pub1', 100000, 10000, 1000, '/upload/img/book/12-14-2008-54-00am-1.png', 1, '2020-11-29 10:53:52', 1, '2020-12-14 14:54:00', 1),
+	(2, 'book2', 'cat1', 'au2', 'lan1', 'pub1', 200000, 10000, 1000, '/upload/img/book/no-image.png', 1, '2020-11-29 10:53:52', 1, '2020-12-03 16:35:14', 1),
+	(3, 'book3', 'cat2', 'au3', 'lan1', 'pub3', 100000, 10000, 1000, '/upload/img/book/no-image.png', 1, '2020-11-29 10:53:52', 1, '2020-12-03 16:35:16', 1),
+	(4, 'book4', 'cat3', 'au1', 'lan2', 'pub3', 300000, 10000, 1000, '/upload/img/book/no-image.png', 1, '2020-11-29 10:53:52', 1, '2020-11-29 10:53:55', 1),
+	(5, 'book5', 'cat3', 'au4', 'lan1', 'pub2', 400000, 10000, 1000, '/upload/img/book/no-image.png', 1, '2020-11-29 10:53:52', 1, '2020-11-29 10:53:55', 1),
+	(6, 'book6', 'cat4', 'au1', 'lan3', 'pub4', 500000, 10000, 1000, '/upload/img/book/no-image.png', 1, '2020-11-29 10:53:52', 1, '2020-11-29 10:53:55', 1),
+	(7, 'book7', 'cat4', 'au1', 'lan1', 'pub1', 200000, 10000, 1000, '/upload/img/book/no-image.png', 1, '2020-11-29 10:53:52', 1, '2020-12-02 11:14:13', 1),
+	(8, 'blah2', 'blah', 'blah', 'en', 'nxb1', 69000, 10000, 20000, '/upload/img/book/no-image.png', 1, '2020-12-02 12:21:04', 1, '2020-12-03 16:35:12', 1),
+	(9, 'test', 'blah', 'blah', 'en', 'nxb1', 50000, 10000, 500, '/upload/img/book/no-image.png', 1, '2020-12-02 12:26:52', 1, '2020-12-03 16:08:46', 1),
+	(10, 'blah23', 'blah', 'blah', 'en', 'nxb1', 69000, 10000, 10, '/upload/img/book/no-image.png', 0, '2020-12-06 20:43:38', 1, '2020-12-06 21:26:16', 1),
+	(17, 'blah235', 'blah', 'blah', 'en', 'nxb1', 54000, 10000, 111, '/upload/img/book/no-image.png', 1, '2020-12-07 11:22:17', 2, '2020-12-07 11:22:17', 2),
+	(18, 'test3434', 'cut1', 'blah', 'vn', 'vn', 50000, 5000, 900, '/upload/img/book/no-image.png', 0, '2020-12-07 11:38:37', 2, '2020-12-07 13:16:36', 1),
+	(21, 'blah2', 'blah', 'blah', 'en', 'nxb1', 69000, 6900, 69, '/upload/img/book/12-14-2008-43-31am-1.png', 1, '2020-12-14 14:43:31', 1, '2020-12-14 14:43:31', 1);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 
 -- Dumping structure for table library.borrow_book
@@ -88,6 +90,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `date_of_birth` date NOT NULL,
   `address` varchar(100) NOT NULL,
   `type` tinyint(4) NOT NULL COMMENT 'Loại tài khoản. 0 - admin, 1 - nhân viên, 2 - thành viên',
+  `image` varchar(150) NOT NULL DEFAULT '/user/no-image.jpg',
   `status` tinyint(4) NOT NULL COMMENT 'Trạng thái tài khoản. 0 - đã xóa, 1 - đang hoạt động',
   `insert_date` datetime NOT NULL,
   `insert_by` int(11) NOT NULL,
@@ -101,14 +104,14 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 -- Dumping data for table library.user: ~7 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`user_id`, `email`, `password`, `name`, `ic_number`, `phone`, `date_of_birth`, `address`, `type`, `status`, `insert_date`, `insert_by`, `update_date`, `update_by`) VALUES
-	(1, 'admin@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'admin', '123', '0333333333', '1990-11-29', 'HN', 0, 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
-	(2, 'employee@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'employee', '124', '0333333331', '1990-11-29', 'HD', 1, 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
-	(3, 'member@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'mem1', '125', '0333333332', '1990-11-29', 'HN', 2, 1, '2020-11-29 10:59:18', 1, '2020-12-07 10:59:23', 2),
-	(4, 'cus2@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'mem2', '126', '0333333334', '1990-11-29', 'HD', 2, 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
-	(5, 'cus3@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'mem3', '127', '0333333335', '1990-11-29', 'HP', 2, 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
-	(6, 'c@a.b', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'test', '1000234532', '0983425321', '1999-03-31', 'HD', 2, 1, '2020-12-07 11:59:08', 2, '2020-12-07 11:59:08', 2),
-	(42, 'cd@a.b', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'test', '1001234532', '0985425321', '1999-03-31', 'HD', 2, 1, '2020-12-07 12:38:42', 2, '2020-12-07 12:40:50', 2);
+INSERT INTO `user` (`user_id`, `email`, `password`, `name`, `ic_number`, `phone`, `date_of_birth`, `address`, `type`, `image`, `status`, `insert_date`, `insert_by`, `update_date`, `update_by`) VALUES
+	(1, 'admin@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'admin', '123', '0333333333', '1990-11-29', 'HN', 0, '/upload/img/user/no-image.png', 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
+	(2, 'employee@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'employee', '124', '0333333331', '1990-11-29', 'HD', 1, '/upload/img/user/no-image.png', 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
+	(3, 'member@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'mem1', '125', '0333333332', '1990-11-29', 'HN', 2, '/upload/img/user/no-image.png', 1, '2020-11-29 10:59:18', 1, '2020-12-07 10:59:23', 2),
+	(4, 'cus2@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'mem2', '126', '0333333334', '1990-11-29', 'HD', 2, '/upload/img/user/no-image.png', 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
+	(5, 'cus3@gmail.com', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'mem3', '127', '0333333335', '1990-11-29', 'HP', 2, '/upload/img/user/no-image.png', 1, '2020-11-29 10:59:18', 1, '2020-11-29 10:59:20', 1),
+	(6, 'c@a.b', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'test', '1000234532', '0983425321', '1999-03-31', 'HD', 2, '/upload/img/user/no-image.png', 1, '2020-12-07 11:59:08', 2, '2020-12-07 11:59:08', 2),
+	(42, 'cd@a.b', 'cb9a7f035d23c91b81d8f9981405d2e566267e4e10ef8ff0722d3d5a61611b52', 'test', '1001234532', '0985425321', '1999-03-31', 'HD', 2, '/upload/img/user/no-image.png', 1, '2020-12-07 12:38:42', 2, '2020-12-07 12:40:50', 2);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
